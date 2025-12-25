a5da2e66c19b003fcc5934f3dd757775ff9f0d8f
Ateret <ateretcohen1@gmail.com>
2025-12-25 16:15:55 +0200

    Diamons

---
+++ Assets/Scripts/UI/DiamondUIManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI; // Required for controlling UI Images
+using TMPro;          // Required for reading the text
+
+public class DiamondUIManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class DiamondPair
+    {
+        public GameObject greyObject;   // The grey diamond (empty)
+        public GameObject purpleObject; // The purple diamond (full/collected)
+    }
+
+    [Header("References")]
+    [Tooltip("Drag the score text here")]
+    public TextMeshProUGUI scoreText;
+
+    [Tooltip("List of diamond pairs")]
+    public DiamondPair[] diamonds;
+
+    private int lastKnownScore = -1;
+
+    void Start()
+    {
+        // Initial setup - reset the display
+        UpdateDiamondsVisibility(0);
+    }
+
+    void Update()
+    {
+        if (scoreText == null) return;
+
+        int currentScore = 0;
+        // Read the score from the text
+        if (int.TryParse(scoreText.text, out currentScore))
+        {
+            // Update only if the number has changed
+            if (currentScore != lastKnownScore)
+            {
+                lastKnownScore = currentScore;
+                UpdateDiamondsVisibility(currentScore);
+            }
+        }
+    }
+
+    void UpdateDiamondsVisibility(int score)
+    {
+        for (int i = 0; i < diamonds.Length; i++)
+        {
+            // If the index is lower than the score, this diamond is collected
+            if (i < score)
+            {
+                // Enable purple, disable grey
+                if (diamonds[i].purpleObject) diamonds[i].purpleObject.SetActive(true);
+                if (diamonds[i].greyObject) diamonds[i].greyObject.SetActive(false);
+            }
+            else
+            {
+                // Enable grey, disable purple (default state)
+                if (diamonds[i].purpleObject) diamonds[i].purpleObject.SetActive(false);
+                if (diamonds[i].greyObject) diamonds[i].greyObject.SetActive(true);
+            }
+        }
+    }
+}
